d8cd436fbcfd8cea6ec581c8a8ce5161e583b744
CarlosEduardoGui <eduardocarlos456@gmail.com>
2021-03-22 23:29:51 -0300

    Começado a padronizar o projeto para ser uma API Restful

---
+++ Alura.WebAPI.WebApp/API/ListasLeituraController.cs
@@ -0,0 +1,50 @@
+﻿using Alura.ListaLeitura.Modelos;
+using Alura.ListaLeitura.Persistencia;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lista = Alura.ListaLeitura.Modelos.ListaLeitura;
+
+namespace Alura.WebAPI.WebApp.API
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ListasLeituraController : ControllerBase
+    {
+        private readonly IRepository<Livro> _repo;
+
+        public ListasLeituraController(IRepository<Livro> repository)
+        {
+            _repo = repository;
+        }
+
+
+        private Lista CriaLista(TipoListaLeitura tipo)
+        {
+            return new Lista
+            {
+                Tipo = tipo.ParaString(),
+                Livros = _repo.All.Where(l => l.Lista == tipo).Select(l => l.ToApi()).ToList()
+            };
+        }
+
+        public IActionResult TodasListas()
+        {
+            Lista paraler = CriaLista(TipoListaLeitura.ParaLer);
+            Lista lendo = CriaLista(TipoListaLeitura.Lendo);
+            Lista lidos = CriaLista(TipoListaLeitura.Lidos);
+            var colecao = new List<Lista> { paraler, lendo, lidos };
+
+            return Ok(colecao);
+        }
+
+        [HttpGet("{tipo}")]
+        public IActionResult Recuperar(TipoListaLeitura tipo)
+        {
+            var lista = CriaLista(tipo);
+            return Ok(lista);
+        }
+    }
+}
